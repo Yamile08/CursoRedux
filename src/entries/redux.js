af35549f8167a724ad3d34ad27d484956f0bd59e
Yamile Tabares Marin <yamile08.lytm@gmail.com>
2018-03-27 12:02:34 -0500

    Actualizando el Store con Reducers

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -29,10 +29,18 @@ const initialState = [
   }
 ]
 
+const reducer = function(state, action) {
+  switch (action.type) {
+    case 'ADD_SONG':
+      return [...state, action.payload]
+    default:
+      return state
+  }
+}
 
 
 const store = createStore( //tres paramentros que recibe el Store
-  (state) => state,
+  reducer,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
